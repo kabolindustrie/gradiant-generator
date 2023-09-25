4666b5b54be8f507c138ad7f610acaa3356e78da
Kabol Amine <amine.kabol@gmail.com>
2023-09-25 12:01:10 +0200

    affichage des dégradés en cour

---
+++ src/utils/getGradientCSSValue.js
@@ -0,0 +1,7 @@
+
+
+export default function getGradientCSSValue(gradient) {
+ const gradientValues = gradient.colors.reduce((acc, obj, index) => acc + `${obj.value} ${obj.position}%${index + 1 !== gradient.colors.length ? "," : ""}`, "")
+
+ return `linear-grandient(${gradient.angle}deg,${gradientValues})`
+}
